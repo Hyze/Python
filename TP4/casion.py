53a873e8ebb49df43fc58f29fea2c9fca7fdce3f
Hyze <alexruault@gmail.com>
2019-02-12 14:32:17 +0100

    fin

--- TP4/casion.py
+++ TP4/casion.py
@@ -50,50 +50,7 @@ class MyImageViewerWidget(QFrame):
 
 
 
-    def Animate(self):
-        for i in range(0, 20):
-            # take care, sleep needs an argument in seconds, not millis
-           # time.sleep((50 + 25 * i) / 1000)  # slow down while the loop runs
 
-
-            if (i < 10):
-
-                a = random.randint(0, 3)
-                self.ui.Centre.setPixmap(QPixmap(self.bigimage.copy(self.rects[a])))
-            if (i < 15):
-                b = random.randint(0, 3)
-                self.ui.Droite.setPixmap(QPixmap(self.bigimage.copy(self.rects[b])))
-
-
-        self.timer.stop()
-        # user = ""
-        # if ((a == b) and (b == c)):
-        #  user = input("You win the jackpot. Press enter to play again or type \"exit\" to exit")
-        #
-        #      #le test ternaire condition ? a : b s'ecrit en pyhton a if condition else b
-        # elif (((1 if (a == self.rects[8]) else 0) + (1 if (b == self.rects[8]) else 0) + (1 if (c == self.rects[8]) else 0)) == 2) :  # check if at least two $
-        #          user = input("You win. Press enter to play again or type \"exit\" to exit")
-        # else :
-        #          user = input("You lose. Press enter to play again or type \"exit\" to exit")
-
-
-
-    def nextG(self):
-
-        c = random.randint(0, 3)
-        self.ui.Gauche.setPixmap(QPixmap(self.bigimage.copy(self.rects[c])))
-        self.timerG.stop()
-        QApplication.processEvents()
-    def nextC(self):
-        a = random.randint(0, 3)
-        self.ui.Centre.setPixmap(QPixmap(self.bigimage.copy(self.rects[a])))
-        self.timerC.stop()
-        QApplication.processEvents()
-    def nextD(self):
-        b = random.randint(0, 3)
-        self.ui.Droite.setPixmap(QPixmap(self.bigimage.copy(self.rects[b])))
-        self.timerD.stop()
-        QApplication.processEvents()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_S:
@@ -135,79 +92,6 @@ class MyImageViewerWidget(QFrame):
             print("You win. ")
         else :
            print("You lose. ")
-
-
-
-
-
-
-        # symbols = ["#", "?", "~", "$"]  # The symbols on the reels
-        #
-        # user = ""
-        # random.seed()
-        # test = int(4 * random.random())
-        #
-        # while (user != "exit"):
-        #
-        #     a=0
-        #     b=0
-        #     c=0
-        #
-        #     for i in range (0,20):
-        #
-        #         # take care, sleep needs an argument in seconds, not millis
-        #         time.sleep( (50 + 25 * i)/1000)   # slow down while the loop runs
-        #
-        #         c = random.randint(0, 3)
-        #
-        #
-        #         if( i < 10 ):
-        #             a =random.randint(0, 3)
-        #
-        #
-        #         if( i < 15 ):
-        #             b = random.randint(0, 3)
-        #
-        #
-        #
-        #    # self.casino()
-        #     cropped3 = self.bigimage.copy(self.rects[c])
-        #     cropped = self.bigimage.copy(self.rects[b])
-        #     cropped2 = self.bigimage.copy(self.rects[a])
-        #     self.ui.Droite.setPixmap(cropped)
-        #     self.ui.Centre.setPixmap(cropped2)
-        #     self.ui.Gauche.setPixmap(cropped3)
-        #     if((a == b) and (b== c)) :
-        #         user = input("You win the jackpot. Press enter to play again or type \"exit\" to exit")
-        #
-        #     # le test ternaire condition ? a : b s'ecrit en pyhton a if condition else b
-        #     elif (((1 if (a == self.rects[8]) else 0) + (1 if (b == self.rects[8]) else 0) + (1 if (c == self.rects[8]) else 0)) == 2) :  # check if at least two $
-        #         user = input("You win. Press enter to play again or type \"exit\" to exit")
-        #     else :
-        #         user = input("You lose. Press enter to play again or type \"exit\" to exit")
-
-
-    def Next(self):
-        if(self.index +1 ==len(self.rects)):
-            self.index=0
-            self.ui.mLabel.setPixmap(QPixmap(self.bigimage.copy(self.rects[self.index])))
-        else:
-             self.index = self.index + 1
-             self.ui.mLabel.setPixmap(QPixmap(self.bigimage.copy(self.rects[self.index])))
-
-
-
-    def Previous(self):
-
-        if(self.index==0):
-
-            self.index=len(self.rects)-1
-            self.ui.mLabel.setPixmap(QPixmap(self.bigimage.copy(self.rects[self.index])))
-
-        else:
-            self.index = self.index-1
-            self.ui.mLabel.setPixmap(QPixmap(self.bigimage.copy(self.rects[self.index])))
-
 
 
 class MyMainWindow(QMainWindow):
